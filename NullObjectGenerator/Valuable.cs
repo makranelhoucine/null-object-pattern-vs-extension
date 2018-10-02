1d2e07a3dfa4e080d5275bf1e871cbad7109b548
julien.vandenbussche@axa.fr <julien.vandenbussche@axa.fr>
2018-10-02 16:00:08 +0200

    first commit

---
+++ NullObjectGenerator/Valuable.cs
@@ -0,0 +1,14 @@
+﻿namespace $rootnamespace$
+{
+    public interface IValuable
+    {
+        bool IsNull { get; }
+    }
+
+    public class Valuable<TNullable> : IValuable where TNullable : Valuable<TNullable>, new()
+    {
+        public static TNullable Null { get; } = new TNullable();
+
+        public bool IsNull => this is TNullable myself;
+    }
+}
